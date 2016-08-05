2b84a180a4cc4e51205df0a60f383e91aadba105
Roland M. Quiros <afictitiousforce@gmail.com>
2016-08-04 20:57:28 -0700

    Added test classes

---
+++ Assets/LostGeneration/Scripts/Tests/Editor/BoardTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace LostGen.Test {
+    public class BoardCommon {
+        public static readonly int[,] GRID_12X8 = new int[,] {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+        };
+    }
+
+    [TestFixture]
+    public class BoardTests {
+        [Test]
+        public void GridWidthAndHeight() {
+            int[,] grid = {
+                {0, 0, 0, 0 },
+                {0, 0, 0, 0 },
+                {0, 0, 0, 0 }
+            };
+
+            Board board = new Board(grid);
+
+            Assert.AreEqual(board.Width, 4);
+            Assert.AreEqual(board.Height, 3);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentException))]
+        public void AddPawn() {
+            Board board = new Board(BoardCommon.GRID_12X8);
+            Pawn pawn = new Pawn("Add", board, new Point(board.Width / 2, board.Height / 2));
+
+            pawn.Offset(Point.One);
+        }
+
+        [Test]
+        public void AreaOfEffect() {
+            
+        }
+    }
+}
---
+++ Assets/LostGeneration/Scripts/Tests/Editor/Skills/WalkTests.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+namespace LostGen.Test {
+    [TestFixture]
+    public class WalkTests {
+        
+        [SetUp]
+        public void Init() {
+
+        }
+
+        [Test]
+        public void PathfindingTest() {
+            Board board = new Board(BoardCommon.GRID_12X8);
+            Combatant combatant = new Combatant("Walker", board, Point.One);
+
+            board.AddPawn(combatant);
+
+            Skills.Walk walk = new Skills.Walk(combatant);
+            combatant.AddSkill(walk);
+
+            Point destination = new Point(10, 6);
+            walk.SetDestination(destination);
+
+            combatant.FireSkill("Walk");
+
+            board.Turn();
+
+            Assert.AreEqual(destination, combatant.Position);
+        }
+    }
+
+}
