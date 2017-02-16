c1319323e52511958341dcdde0613fb852bd58de
Roland M. Quiros <afictitiousforce@gmail.com>
2017-02-16 00:16:13 -0800

    Working on base character model animations, PawnMessage processing

---
+++ Assets/LostGeneration/Scripts/View/CombatantView.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LostGen;
+
+public class CombatantView : MonoBehaviour {
+	#region EditorFields
+	/// <summary>Time needed to move one tile</summary>
+	public float MoveDuration = 0.5f;
+	#endregion EditorFields
+	public Pawn Pawn;
+
+	public void ProcessMessage(IPawnMessage message) {
+		// Check the type of the message, then fire off the appropriate Coroutine
+		if (Pawn == message.Source) {
+			MoveMessage move = message as MoveMessage;
+			if (move != null) {
+				StartCoroutine(Move(move));
+			}
+		}
+	}
+
+	private IEnumerator Move(MoveMessage move) {
+		Vector3 from = PointVector.ToVector(move.From);
+		Vector3 to = PointVector.ToVector(move.To);
+
+		Vector3 step = (to - from) / MoveDuration;
+		float time = 0f;
+
+		while (time < MoveDuration) {
+			time += Time.deltaTime;
+			transform.position = transform.position + (step * Time.deltaTime);
+			yield return null;
+		}
+
+		transform.position = to;
+	}
+
+	#region MonoBehaviour
+	private void Awake() {
+		
+	}
+	#endregion MonoBehaviour
+}
---
+++ Assets/LostGeneration/Scripts/View/PawnManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using LostGen;
+
+public class PawnManager : MonoBehaviour {
+    public GameObject CombatantPrefab;
+    private Dictionary<Pawn, GameObject> _pawnObjects = new Dictionary<Pawn, GameObject>();
+    public void OnPawnAdded(Pawn pawn) {
+        if (!_pawnObjects.ContainsKey(pawn)) {
+            Combatant combatant = pawn.GetComponent<Combatant>();
+            if (combatant != null) {
+                GameObject combatantObject = GameObject.Instantiate(CombatantPrefab, PointVector.ToVector(pawn.Position), Quaternion.identity);
+
+                // Here, we need to pull Character information and apply it to the MonoBehaviours in combatantObject
+                CombatantView view = combatantObject.GetComponent<CombatantView>();
+                if (view == null) {
+                    throw new Exception("Assigned Combatant Prefab does not have a CombatantView component");
+                }
+                view.Pawn = pawn;
+                
+                // Then, attach the MonoBehaviours to the MessageBuffer
+
+
+                _pawnObjects.Add(pawn, combatantObject);
+            }
+        }
+    }
+}
