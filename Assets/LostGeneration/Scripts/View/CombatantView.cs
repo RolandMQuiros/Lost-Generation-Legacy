762ca43ced6d0935b85d03bd05bba3cfdfc6984b
Roland M. Quiros <afictitiousforce@gmail.com>
2017-02-16 00:28:56 -0800

    Added a Finished event to CombatantView. Removed some fluff from the BaseHuman export.

--- Assets/LostGeneration/Scripts/View/CombatantView.cs
+++ Assets/LostGeneration/Scripts/View/CombatantView.cs
@@ -1,22 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using LostGen;
 
 public class CombatantView : MonoBehaviour {
 	#region EditorFields
-	/// <summary>Time needed to move one tile</summary>
+	/// <summary>Time needed to move across one block</summary>
+	[TooltipAttribute("Time needed to move across one block")]
 	public float MoveDuration = 0.5f;
+	/// <summary>Invoked when this CombatantView's coroutines finish.</summary>
+	[TooltipAttribute("Invoked when this CombatantView's coroutines finish")]
+	public UnityEvent Finished;
 	#endregion EditorFields
 	public Pawn Pawn;
-
+	private Dictionary<IPawnMessage, Coroutine> _activeRuns = new Dictionary<IPawnMessage, Coroutine>();
+	
 	public void ProcessMessage(IPawnMessage message) {
 		// Check the type of the message, then fire off the appropriate Coroutine
 		if (Pawn == message.Source) {
 			MoveMessage move = message as MoveMessage;
 			if (move != null) {
-				StartCoroutine(Move(move));
+				Coroutine moveCo = StartCoroutine(Move(move));
+				_activeRuns.Add(move, moveCo);
 			}
+		}
+	}
+
+	private void Finish(IPawnMessage message) {
+		// Removes the message from the list of active Coroutines
+		_activeRuns.Remove(message);
+		if (_activeRuns.Count == 0) {
+			Finished.Invoke();
 		}
 	}
 
@@ -34,6 +49,8 @@ public class CombatantView : MonoBehaviour {
 		}
 
 		transform.position = to;
+
+		Finish(move);
 	}
 
 	#region MonoBehaviour
