6fd978301bd90b756910da42e4eae6680758690b
Roland M. Quiros <afictitiousforce@gmail.com>
2017-03-12 20:38:09 -0700

    Added base toon shader, adjusted placeholder model

--- Assets/LostGeneration/Scripts/Model/DjikstraMap.cs
+++ Assets/LostGeneration/Scripts/Model/DjikstraMap.cs
@@ -3,13 +3,13 @@
 namespace LostGen {
 
 	public class DjikstraMap {
-		private Board _board;
-		private byte[,,] _values;
+		public Board Board { get; private set; }
+		private int[,,] _values;
 		private Predicate<Point> _filter;
 
 		public DjikstraMap(Board board, Predicate<Point> filter = null) {
-			_board = board;
-			_values = new byte[board.Size.X, board.Size.Y, board.Size.Z];			
+			Board = board;
+			_values = new int[board.Size.X, board.Size.Y, board.Size.Z];			
 			_filter = filter ?? DefaultFilter;
 			
 			Clear();
@@ -17,26 +17,26 @@ namespace LostGen {
 
 		public void Clear() {
 			// Initialize values cell to max
-			for (int x = 0; x < _board.Size.X; x++) {
-				for (int y = 0; y < _board.Size.Y; y++) {
-					for (int z = 0; z < _board.Size.Z; z++) {
-						_values[x, y, z] = Byte.MaxValue;
+			for (int x = 0; x < Board.Size.X; x++) {
+				for (int y = 0; y < Board.Size.Y; y++) {
+					for (int z = 0; z < Board.Size.Z; z++) {
+						_values[x, y, z] = Int32.MaxValue;
 					}
 				}
 			}
 		}
 
-		public void SetValue(Point point, byte value) {
-			if (_board.InBounds(point)) {
+		public void SetValue(Point point, int value) {
+			if (Board.InBounds(point)) {
 				_values[point.X, point.Y, point.Z] = value;
 			} else {
 				throw new ArgumentOutOfRangeException("blockPoint", "Given point is outside the board");
 			}
 		}
 
-		public byte GetValue(Point point) {
-			byte value;
-			if (_board.InBounds(point)) {
+		public int GetValue(Point point) {
+			int value;
+			if (Board.InBounds(point)) {
 				value = _values[point.X, point.Y, point.Z];
 			} else {
 				throw new ArgumentOutOfRangeException("blockPoint", "Given point is outside the board");
@@ -51,19 +51,19 @@ namespace LostGen {
 			
 			while (changed || (maxPasses > 0 && passes < maxPasses)) {
 				changed = false;
-				for (int x = 0; x < _board.Size.X; x++) {
-					for (int y = 0; y < _board.Size.Y; y++) {
-						for (int z = 0; z < _board.Size.Z; z++) {
+				for (int x = 0; x < Board.Size.X; x++) {
+					for (int y = 0; y < Board.Size.Y; y++) {
+						for (int z = 0; z < Board.Size.Z; z++) {
 							Point point = new Point(x, y, z);
 							int value = _values[x, y, z];
 
 							if (!_filter(point)) {
 								// Find the neighbor with the smallest value
-								byte min = Byte.MaxValue;
+								int min = Int32.MaxValue;
 								for (int i = 0; i < Point.Neighbors.Length; i++) {
 									Point neighbor = point + Point.Neighbors[i];
-									if (_board.InBounds(neighbor)) {
-										byte neighborVal = _values[neighbor.X, neighbor.Y, neighbor.Z];
+									if (Board.InBounds(neighbor)) {
+										int neighborVal = _values[neighbor.X, neighbor.Y, neighbor.Z];
 										if (neighborVal < min) {
 											min = neighborVal;
 										}
@@ -72,7 +72,7 @@ namespace LostGen {
 
 								// If the current block's value is at least 2 greater than the minimum, set it to 1 more than that minimum
 								if (value - min > 2) {
-									_values[x, y, z] = (byte)(1 + min);
+									_values[x, y, z] = min + 1;
 									changed = true;
 								}
 							}
@@ -82,17 +82,17 @@ namespace LostGen {
 				passes++;
 			}
 		}
-
+		
 		private bool DefaultFilter(Point point) {
 			bool skip = false;
 
 			// Skip only if the current block is solid, or the block below it is not solid.
 			// This limits the scan to blocks that have ground to stand on.
-			if (_board.InBounds(point)) {
-				BoardBlock block = _board.GetBlock(point);
+			if (Board.InBounds(point)) {
+				BoardBlock block = Board.GetBlock(point);
 				skip = block.IsSolid;
-				if (!skip && _board.InBounds(point + Point.Down)) {
-					BoardBlock below = _board.GetBlock(point + Point.Down);
+				if (!skip && Board.InBounds(point + Point.Down)) {
+					BoardBlock below = Board.GetBlock(point + Point.Down);
 					skip = !below.IsSolid;
 				}
 			}
@@ -102,13 +102,13 @@ namespace LostGen {
 
 		#region OperatorOverloads
 
-		public static DjikstraMap operator +(DjikstraMap map, byte amount) {
-			DjikstraMap sum = new DjikstraMap(map._board, map._filter);
+		public static DjikstraMap operator +(DjikstraMap map, int amount) {
+			DjikstraMap sum = new DjikstraMap(map.Board, map._filter);
 			Array.Copy(map._values, sum._values, map._values.Length);
 
-			for (int x = 0; x < sum._board.Size.X; x++) {
-				for (int y = 0; y < sum._board.Size.Y; y++) {
-					for (int z = 0; z < sum._board.Size.Z; z++) {
+			for (int x = 0; x < sum.Board.Size.X; x++) {
+				for (int y = 0; y < sum.Board.Size.Y; y++) {
+					for (int z = 0; z < sum.Board.Size.Z; z++) {
 						sum._values[x, y, z] += amount;
 					}
 				}
@@ -117,27 +117,15 @@ namespace LostGen {
 			return sum;
 		}
 
-		public static DjikstraMap operator +(DjikstraMap map, int amount) {
-			return map + (byte)amount;
-		}
-
-		public static DjikstraMap operator -(DjikstraMap map, byte amount) {
-			return map + (-amount);
-		}
-
-		public static DjikstraMap operator -(DjikstraMap map, int amount) {
-			return map + (byte)(-amount);
-		}
-
 		public static DjikstraMap operator *(DjikstraMap map, float multiplier) {
-			DjikstraMap sum = new DjikstraMap(map._board, map._filter);
+			DjikstraMap sum = new DjikstraMap(map.Board, map._filter);
 			Array.Copy(map._values, sum._values, map._values.Length);
 
-			for (int x = 0; x < sum._board.Size.X; x++) {
-				for (int y = 0; y < sum._board.Size.Y; y++) {
-					for (int z = 0; z < sum._board.Size.Z; z++) {
-						byte value = sum._values[x, y, z];
-						sum._values[x, y, z] = (byte)(multiplier * value + 0.5f);
+			for (int x = 0; x < sum.Board.Size.X; x++) {
+				for (int y = 0; y < sum.Board.Size.Y; y++) {
+					for (int z = 0; z < sum.Board.Size.Z; z++) {
+						int value = sum._values[x, y, z];
+						sum._values[x, y, z] = (int)(multiplier * value + 0.5f);
 					}
 				}
 			}
--- Assets/LostGeneration/Scripts/Model/Tests/Editor/DjikstraMapTests.cs
+++ Assets/LostGeneration/Scripts/Model/Tests/Editor/DjikstraMapTests.cs
@@ -6,20 +6,43 @@ namespace Tests {
 
 	public class DjikstraMapTests {
 
+		private void PrintMap(DjikstraMap map) {
+			string display = string.Empty;
+			for (int y = 0; y < map.Board.Size.Y; y++) {
+				for (int z = 0; z < map.Board.Size.Z; z++) {
+					for (int x = 0; x < map.Board.Size.X; x++) {
+						Point point = new Point(x, y, z);
+						if (map.Board.GetBlock(point).IsSolid) {
+							display += '#';
+						} else {
+							display += map.GetValue(point).ToString("X");
+						}
+					}
+					display += '\n';
+				}
+				display += "\n\n";
+			}
+			Console.WriteLine(display);
+		}
+
 		[Test]
 		public void ConstructTest() {
 			Board board = BoardCommon.ArrayToBoard( new int[,,] {
 				{
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 },
-					{ 0, 0, 0, 0 }
+					{ 0, 0, 0, 0, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 0, 0, 0, 0 },
+					{ 0, 0, 0, 0, 0, 0, 0, 0 }
 				},
 				{	
-					{ 0, 0, 0, 1 },
-					{ 1, 1, 0, 1 },
-					{ 0, 1, 1, 1 },
-					{ 0, 1, 0, 0 }
+					{ 0, 0, 0, 0, 0, 1, 1, 0 },
+					{ 0, 1, 0, 0, 0, 1, 1, 1 },
+					{ 0, 1, 1, 1, 1, 1, 1, 0 },
+					{ 1, 1, 1, 0, 1, 1, 1, 1 },
+					{ 1, 1, 0, 1, 1, 1, 1, 1 },
+					{ 0, 0, 0, 0, 0, 0, 1, 0 }
 				}
 			});
 
@@ -27,23 +50,7 @@ namespace Tests {
 			map.SetValue(new Point(1, 1, 2), 0);
 			map.Build(1);
 
-			string display = string.Empty;
-			for (int y = 0; y < board.Size.Y; y++) {
-				for (int z = 0; z < board.Size.Z; z++) {
-					for (int x = 0; x < board.Size.X; x++) {
-						Point point = new Point(x, y, z);
-						if (board.GetBlock(point).IsSolid) {
-							display += '#';
-						} else {
-							display += map.GetValue(point);
-						}
-					}
-					display += '\n';
-				}
-				display += "\n\n";
-			}
-
-			Console.WriteLine(display);
+			PrintMap(map);
 		}
 	}
 }
