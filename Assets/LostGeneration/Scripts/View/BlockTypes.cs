31c4f824a51f4e9821ec1528d37679904c94923b
Roland Quiros <Roland Quiros>
2017-05-27 14:32:33 -0700

    Added BlockTypes

---
+++ Assets/LostGeneration/Scripts/View/BlockTypes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(fileName = "BlockProperties", menuName = "Lost Generation/BlockProperties")]
+public class BlockProperties : ScriptableObject {
+	public Sprite TopSprite;
+	public Sprite RightSprite;
+	public Sprite DownSprite;
+	public Sprite LeftSprite;
+	public Sprite ForwardSprite;
+	public Sprite BackwardSprite;
+
+	public Sprite[] SideSprites
+	{
+		get { return _sideSprites; }
+	}
+
+	private Sprite[] _sideSprites;
+	private void OnEnable()
+	{
+		_sideSprites = new Sprite[]
+		{
+			TopSprite,
+			RightSprite,
+			DownSprite,
+			LeftSprite,
+			ForwardSprite,
+			BackwardSprite
+		};
+	}
+}
